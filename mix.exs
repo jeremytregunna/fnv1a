715f4accc715f77cf3b01a7956b4ace956078812
Jeremy Tregunna <jeremy@tregunna.ca>
2017-04-03 14:59:53 -0600

    make things ready for hex

--- mix.exs
+++ mix.exs
@@ -7,15 +7,25 @@ defmodule Fnv1a.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     package: package()]
   end
 
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
     [extra_applications: [:logger]]
   end
 
   defp deps do
-    []
+    [{:ex_doc, "~> 0.15.0", only: :dev}]
+  end
+
+  defp package do
+    [name: :fnv1a,
+     description: "FNV-1a 32-bit hash algorithm implementation",
+     files: ~w(lib mix.exs README.md LICENSE LICENSE-2.0),
+     maintainers: ["Jeremy Tregunna"],
+     licenses: ["Apache 2.0"],
+     links: %{"GitHub" => "https://github.com/jeremytregunna/fnv1a.git",
+              "Docs" => "https://hexdocs.pm/fnv1a"}]
   end
 end
